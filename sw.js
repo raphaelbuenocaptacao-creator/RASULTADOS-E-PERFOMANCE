0ec4d319e26b29127e68bb171cd40c959c9cd72f
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-17 09:24:55 -0300

    Adiciona service worker PWA

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='bueno-dashboard-v1';
+self.addEventListener('install',event=>{self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))) });
