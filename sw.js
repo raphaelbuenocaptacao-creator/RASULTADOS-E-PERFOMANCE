1725dfd05244b6a4f3823261b0753ef696ae5f4f
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-20 21:00:53 -0300

    Renova cache do dashboard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vt-hub-trimestral-v3';
+const CACHE='vt-hub-trimestral-v5';
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','dashboard-data.json','manifest.webmanifest'])));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request)))});
